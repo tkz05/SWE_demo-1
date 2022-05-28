a927e937b7e0754267f0bf15e49f378d6105acb5
dietaler <tomtom21107@gmail.com>
2022-05-28 20:03:26 +0800

    original code

---
+++ C_err.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int check_user(char *,char *);
+void insert(void);
+void hacker(void);
+char pass[]="1030451",username[]="ADMIN";
+
+int main(int argc,char**argv)
+{
+
+}
+int check_user(char *in1,char *in2)
+{
+
+}
+void insert(void)
+{
+
+}
+void hacker(void)
+{
+
+}
